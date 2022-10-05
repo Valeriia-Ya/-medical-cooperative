6a1041946d1529caa9202539eff4909a9e30d524
ElizavetaKropaleva <9115437845l@gmail.com>
2022-10-05 11:21:26 +0300

    Проект с главной страницей

---
+++ medical/medical_cooperative/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'medical_cooperative'
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('inspections/', views.inspections, name='inspections')
+]
